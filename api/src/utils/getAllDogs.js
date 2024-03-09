e8e80367335efe8b6745087a2c778ea48d51718a
GermanEH <germanhdev@gmail.com>
2024-03-09 16:18:13 -0300

    Feat. Add conditional bulkmigration of Dogs from API to database

--- api/src/utils/getAllDogs.js
+++ api/src/utils/getAllDogs.js
@@ -4,10 +4,22 @@ const { Op } = require('sequelize');
 const { Dog, Temperament } = require('../db.js');
 const api = require('../helpers/dog_api.js');
 
-async function getAllDogs(name) {
+
+const checkForLocalDogs = async (name) => {
+
   try {
-    if (name) {
-      let localDogs = await Dog.findAll({
+    let localDogs
+    if(!name) {
+
+      localDogs = await Dog.findAll()
+      
+      const dogs = localDogs.map(localDog => localDog.dataValues)
+
+      return dogs
+  
+    } else if (name) {
+  
+      const dogs = await Dog.findAll({
         where: { name: { [Op.substring]: name } },
         include: {
           attributes: ['name'],
@@ -21,121 +33,101 @@ async function getAllDogs(name) {
             id: d.ID,
             image: d.image,
             name: d.name,
-            weight: d.weight.metric,
             temperament: temperament,
-            origin: ['created'],
+            weight: d.weight,
+            // origin: ['created'],
           };
         })
-      );
-      let someApiDogs = [];
-      await axiosFunction({
-        url: `${api.SEARCH}${name}`,
-        cbSuccess: (response) => {
-          someApiDogs = response.data.map(async (d) => {
-            const dogs = await axios.get(`${api.BREEDS}`);
-            let image = '';
-            if (d.reference_image_id) {
-              const dog = await dogs.data.find(
-                (dg) => dg.image.id === d.reference_image_id
-              );
-              image = dog.image.url;
-              if (d.weight.metric === 'NaN')
-                d.weight.metric = `${Math.floor(
-                  d.weight.imperial.split(' ', 1) / 2.25
-                )} - ${Math.floor(d.weight.imperial.split(' ').pop() / 2.25)}`; // hay un perro que viene con weight.metric como NaN
-              if (d.temperament) {
-                d.temperament = d.temperament.split(', ');
-              } else {
-                d.temperament = ['not temperament available '];
-              } //lo hago para emparejar el tratamiento que DogCard hace con los casos de getAllDogs SIN name (que son arreglo de arreglos para poder hacer filtros combinados)
-              return {
-                image: image,
-                name: d.name,
-                temperament: d.temperament,
-                weight: d.weight.metric,
-                origin: ['original'],
-              };
-            }
-            if (d.weight.metric === 'NaN')
-              d.weight.metric = `${Math.floor(
-                d.weight.imperial.split(' ', 1) / 2.25
-              )} - ${Math.floor(d.weight.imperial.split(' ').pop() / 2.25)}`; // hay un perro que viene con weight.metric como NaN
-            if (d.temperament) {
-              d.temperament = d.temperament.split(', ');
-            } else {
-              d.temperament = ['not temperament available '];
-            } //lo hago para emparejar el tratamiento que DogCard hace con los casos de getAllDogs SIN name (que son arreglo de arreglos para poder hacer filtros combinados)
-            return {
-              //algunos de estos perros no tienen imagen (no aparecen en breeds tampoco) //también podría haber puesto image - defaultValue: null en el modelo de Dog
-              image: 'not image available',
-              name: d.name,
-              temperament: d.temperament,
-              weight: d.weight.metric,
-              origin: ['original'],
-            };
-          });
-        },
-        arr: someApiDogs,
-      });
-
-      return Promise.all([...localDogs, ...someApiDogs]);
+      ); 
+  
+      return Promise.all([...dogs])
+  
     }
-    if (!name) {
-      let localDogs = await Dog.findAll({
-        include: {
-          attributes: ['name'],
-          as: ['temperament'],
-          model: Temperament,
-        },
-      }).then((data) =>
-        data.map((d) => {
-          let temperament = d.temperaments.map((t) => t.name);
-          return {
-            id: d.ID,
-            image: d.image,
-            name: d.name,
-            weight: d.weight.metric,
-            temperament: temperament,
-            origin: ['created'],
-          };
-        })
-      );
-      let apiDogs = [];
-      await axiosFunction({
-        url: `${api.BREEDS}?limit=25`,
-        cbSuccess: (response) => {
-          apiDogs = response.data.map(async (d) => {
-            if (d.weight.metric === 'NaN')
-              d.weight.metric = `${Math.floor(
-                d.weight.imperial.split(' ', 1) / 2.25
-              )} - ${Math.floor(d.weight.imperial.split(' ').pop() / 2.25)}`; // hay un perro que viene con weight.metric como NaN
-            let temperament = d.temperament.split(', '); //es un string, no un arreglo (para poder hacer los filtros combinados necesito que cada temperamento sea un array independiente dentro del array padre)
-            let response = await axios.get(
-              `${api.IMAGES}/${d.reference_image_id}`
-            );
-            return {
-              id: d.id,
-              image: response.data.url,
-              name: d.name,
-              temperament: temperament,
-              weight: d.weight.metric,
-              origin: ['original'],
-            };
-          });
-        },
-        arr: apiDogs,
-      });
-      // console.log('localDogs: ', localDogs);
-      return Promise.all([...apiDogs, ...localDogs]);
-    }
+    
   } catch (error) {
-    console.log(error);
-    return { error: error.message };
+    
+    return {error: error.message}
+
   }
-  // finally {
-  //   console.log(error);
-  //   console.log('done');
-  // }
+  
+  
 }
 
+const getApiDogs = async () => {
+
+  try {
+
+  let apiDogs = [];
+
+  await axiosFunction({
+    url: `${api.BREEDS}?limit=25`,
+    cbSuccess: (response) => {
+      apiDogs = response.data.map(async (d) => {
+        if (d.weight.metric === 'NaN')
+          d.weight.metric = `${Math.floor(
+            d.weight.imperial.split(' ', 1) / 2.25
+          )} - ${Math.floor(d.weight.imperial.split(' ').pop() / 2.25)}`; // hay un perro que viene con weight.metric como NaN
+        let temperament = d.temperament.split(', '); //es un string, no un arreglo (para poder hacer los filtros combinados necesito que cada temperamento sea un array independiente dentro del array padre)
+        let response = await axios.get(
+          `${api.IMAGES}/${d.reference_image_id}`
+        );
+        return {
+          id: d.id,
+          image: response.data.url,
+          name: d.name,
+          temperament: temperament,
+          weight: d.weight.metric,
+          height: d.height.metric,
+          life_span: d.life_span
+        };
+      });
+    },
+    arr: apiDogs,
+  });
+  
+  return Promise.all([...apiDogs])
+
+  } catch (error) {
+    
+    return { error: error.message };
+
+  }
+  
+}
+
+const saveToDataBase = async (data) => {
+  
+  const newLocalDogs = await Dog.bulkCreate(data)
+
+  const dogs = newLocalDogs.map(newLocalDog => newLocalDog.dataValues)
+
+  return dogs
+
+}
+
+async function getAllDogs(name) {
+
+  try {
+
+    let dogs = await checkForLocalDogs()
+
+    if(dogs.length === 0) {
+
+      const data = await getApiDogs(name)
+
+      dogs = await saveToDataBase(data)
+
+      return await checkForLocalDogs()
+
+    }
+
+    return dogs
+
+  } catch (error) {
+
+    return { error: error.message };
+
+  }
+
+}
 module.exports = getAllDogs;
