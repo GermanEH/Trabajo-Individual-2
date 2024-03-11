de10e5b9d015e7947e36ced7f1d7a37b7b94945b
GermanEH <germanhdev@gmail.com>
2024-03-11 18:51:36 -0300

    Chore. Change imports and refactor controllers code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,101 +1,14 @@
-const { Router } = require('express');
-const { Dog, Temperament } = require('../db.js');
+import { Router } from 'express';
+import { getAllDogs, getDogById, postDog } from '../controllers/dogsController.js';
+import { getTemperaments } from '../controllers/temperamentsController.js';
 
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+export const router = Router();
 
-const axios = require('axios');
-const router = Router();
 
-const getAllDogs = require('../utils/getAllDogs');
-const getDog = require('../utils/getDog');
-const getTemperaments = require('../utils/getTemperaments');
-const postDog = require('../utils/postDog');
+router.get('/dogs/:id', getDogById);
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+router.post('/dogs', postDog);
 
-router.get('/dogs', async (req, res) => {
-  const { name } = req.query;
-  try {
-    if (name) {
-      //listado de las razas de perro que contengan la palabra ingresada
-      const response = await getAllDogs(name);
-      return res.status(201).json(response);
-    }
-    const response = await getAllDogs();
-    return res.status(201).json(response);
-  } catch (error) {
-    return res.status(404).send({ error: error.message });
-  } finally {
-    console.log('done');
-  }
-});
+router.get('/temperaments', getTemperaments);
 
-router.get('/dogs/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const response = await getDog(id);
-    res.status(201).send(response);
-  } catch (error) {
-    res.status(404).send({ error: error.message });
-  } finally {
-    console.log('done');
-  }
-});
-router.post('/dogs', async (req, res) => {
-  const {
-    name,
-    minHeight,
-    maxHeight,
-    minWeight,
-    maxWeight,
-    minLifeSpan,
-    maxLifeSpan,
-    image,
-    temperament,
-  } = req.body;
-  if (!name || !temperament) {
-    throw new Error('Missing dog name or Temperament');
-  }
-
-  let height = `${[minHeight, maxHeight]}`;
-  let weight = `${[minWeight, maxWeight]}`;
-  let life_span = `${[minLifeSpan, maxLifeSpan]}`;
-
-  let temperaments = [];
-  for (const t of temperament) {
-    const selectedTemperament = await Temperament.findOne({
-      where: { name: t },
-    });
-    temperaments.push(selectedTemperament.id);
-  }
-  try {
-    const dog = await postDog(
-      name,
-      height,
-      weight,
-      life_span,
-      image,
-      temperaments
-    );
-    res.status(201).send(dog);
-  } catch (error) {
-    res.status(404).send({ error: error.message });
-  } finally {
-    console.log('done');
-  }
-});
-router.get('/temperaments', async (req, res) => {
-  try {
-    console.log('entramos a temperaments');
-    const response = await getTemperaments();
-    res.status(201).send(response);
-  } catch (error) {
-    res.status(404).send({ error: error.message });
-  } finally {
-    console.log('done');
-  }
-});
-
-module.exports = router;
+router.get('/dogs', getAllDogs);
