d4d07e1de8412e4d744bf8e756654528a45e89b1
GermanEH <germanhdev@gmail.com>
2024-03-12 18:03:40 -0300

    Add. Update Filters component with latest changes to Home

--- client/src/components/renders/Sorters.jsx
+++ client/src/components/renders/Sorters.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import style from '../../assets/styles/Sorters.module.css' 
 
-export default function Sorters ({sortByName, sortByWeight, dispatch, filtered}) {
+export default function Sorters ({sorters, setSorters, dispatch, filtered}) {
 
     return (
+        <div className={style.column_right}>
             <div className={style.sorters}>
                 <h3 className={style.sorters_title}>Ordenar por...</h3>
                         <h4 className={style.sorters_subtitle}>Nombre</h4>
-                    <select className={style.options} id="origins" onChange={(e) => dispatch(sortByName(e.target.value, filtered))}>
+                    <select className={style.options} id="name" onChange={(e) => setSorters(prevState => ({...prevState, [e.target.id]:e.target.value}))}>
                         <option defaultValue hidden>elige...</option>   
                         <option className={style.option} value="asc">ascendente</option>
                         <option className={style.option} value="desc">descendente</option>
                     </select>
                         <h4 className={style.sorters_subtitle}>Peso</h4>
-                    <select className={style.options} id="weight" onChange={(e) => dispatch(sortByWeight(e.target.value, filtered))}>
+                    <select className={style.options} id="weight" onChange={(e) => setSorters(prevState => ({...prevState, [e.target.id]:e.target.value}))}>
                         <option defaultValue hidden>elige...</option>
                         <option className={style.option} value="asc">ascendente</option>
                         <option className={style.option} value="desc">descendente</option>
                     </select>
             </div>
+        </div>
     )
 }
 
