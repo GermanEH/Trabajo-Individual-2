696b7a5f8f3ca327f1f1394582751892eec5d43f
GermanEH <germanhdev@gmail.com>
2024-03-14 15:13:36 -0300

    Feat. Implement new NavBar in Breed component

---
+++ client/src/pages/Breeds/Breeds.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../../components/containers/NavBar'
+import Home from '../../components/containers/Home'
+
+const Breeds = () => {
+  return (
+    <>
+    <NavBar/>
+    <Home/>
+    </>
+  )
+}
+
+export default Breeds
