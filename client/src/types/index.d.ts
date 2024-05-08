f1fb5fa19090e276a4c8c0de94235ba3c9aa2472
GermanEH <germanhdev@gmail.com>
2024-05-07 22:48:40 -0300

    Chore. Add paths src/*

---
+++ client/src/types/index.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="vite/client"/>
+declare module '*.jpg' 
+declare module '*.js'
+// // {
+// //     const value: string;
+// //     export const value;
+// //   }
+// declare module '@/store/user/useUserStore'
+// declare module '@/store/StylesEditor/useStateStyle'
+// declare module '@/store/tabStore/useTabStore'
+// declare module '@/store/templateStore/useTemplateStore'
+// declare module '@/store/index'
+// declare module '@/routes/core/guard/isNotLogged'
+// declare module '@/routes/core/guard/isLogged'
+// declare module '@/routes/routes'
+// declare module '@/types/user'
+
+export * from './dog.d'
+
